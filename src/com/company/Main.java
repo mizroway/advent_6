d944221ffdb76b5eadae425fa2011ee1e23f5720
tomekmizerski <tomasz_mizerski@interia.pl>
2020-12-08 22:30:40 +0100

    part 1 works

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,35 @@
 package com.company;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.regex.Matcher;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        ArrayList<String> input = new ArrayList<>(Files.readAllLines(Paths.get("input_6.txt")));
+        Set<Character> answers = new HashSet<>();
+//        Map<Integer, Set<Character>> mapa = new HashMap<>();
+
+        int counter = 0;
+
+        for (int i = 0; i < input.size(); i++) {
+
+            char[] tab = input.get(i).toCharArray();
+            for (char c : tab) {
+                answers.add(c);
+
+            }
+
+            if (input.get(i).isEmpty() || i == input.size() - 1) {
+//                mapa.put(counter, interMapa);
+                System.out.println("answers = " + answers);
+                counter += answers.size();
+                System.out.println("counter = " + counter);
+                answers = new HashSet<>();
+            }
+        }
     }
 }
